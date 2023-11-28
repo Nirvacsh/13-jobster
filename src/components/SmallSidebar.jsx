6f3a3f3818afe91434f39d35e1f637fb0c90ee55
Nirvacsh <nir.vlad.zu@gmail.com>
2023-11-28 18:08:18 +0200

    #3 task 37 done

---
+++ src/components/SmallSidebar.jsx
@@ -0,0 +1,10 @@
+import Wrapper from '../assets/wrappers/SmallSidebar.js'
+
+const SmallSidebar = () => {
+  return (
+    <Wrapper>
+      <h2>Small</h2>
+    </Wrapper>
+  )
+}
+export default SmallSidebar
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FormRow, Logo } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
+import { toast } from 'react-toastify'
+import { useDispatch, useSelector } from 'react-redux'
+import { loginUser, registerUser } from '../features/user/userSlice'
+import { useNavigate } from 'react-router-dom'
 
 const initialState = {
   name: '',
@@ -10,19 +14,38 @@ const initialState = {
 }
 const Register = () => {
   const [values, setValues] = useState(initialState)
+  const { user, isLoading } = useSelector((store) => store.user)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const onSubmit = (e) => {
     e.preventDefault()
-    console.log(e.target)
+    const { name, email, password, isMember } = values
+    if (!email || !password || (!isMember && !name)) {
+      toast.error('Please fill out all fields')
+      return
+    }
+
+    if (isMember) {
+      dispatch(loginUser({ email, password }))
+      return
+    }
+    dispatch(registerUser({ name, email, password }))
   }
 
   const handleChange = (e) => {
-    console.log(e.target)
+    const name = e.target.name
+    const value = e.target.value
+    setValues({ ...values, [name]: value })
   }
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember })
   }
+
+  useEffect(() => {
+    navigate('/')
+  }, [user, navigate])
 
   return (
     <Wrapper className='full-page'>
@@ -50,8 +73,8 @@ const Register = () => {
           value={values.password}
           handleChange={handleChange}
         />
-        <button type='submit' className='btn btn-block'>
-          Submit
+        <button type='submit' className='btn btn-block' disabled={isLoading}>
+          {isLoading ? 'Loading ...' : 'Submit'}
         </button>
         <p>
           {values.isMember ? 'Not a member yet?' : 'Already a member?'}
